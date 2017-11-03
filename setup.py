f4b4204a4138373765fcae8a77329dea030b0679
Zach Pincus <zpincus@gmail.com>
2017-11-03 14:19:42 -0500

    initial code commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name = 'elegant',
+    version = '1',
+    description = 'tools and pipelines for zplab C. elegans data',
+    packages = setuptools.find_packages(),
+)
