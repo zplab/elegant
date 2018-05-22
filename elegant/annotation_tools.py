7a6d8693e4777d38897590c725a7130ff10d8f14
drew-sinha <>
2018-05-22 09:15:39 -0500

    Added check_complete_annotations (unstable)

--- elegant/annotation_tools.py
+++ elegant/annotation_tools.py
@@ -25,5 +25,34 @@ def filter_positions_by_kw(annotations, selection_kws, invert_selection=False):
             for position in selected_positions 
             if position not in annotations.keys()]
 
+def check_complete_annotations(annotations, stages):
+    """Check that a set of annotations are complete 
+        
+        Parameters
+            annotations - An OrderedDict mapping position names to corresponding
+                annotations (returned by load_data.read_annotations)
+            stages - A iterable containing the stages that should be annotated
+                for this experiment (e.g. could be ('larva','adult','dead')
+                for a complete experiment, but only ('larva', 'adult') for 
+                an ongoing experiment)
+        Returns
+            bad_positions - a list of positions with incomplete annotations
+    """
+    
+    bad_positions = []
+    for position_name, position_annotations in annotations.items():
+        global_annotations, timepoint_annotations = position_annotations
+        
+        missing_stages = []
+        for stage in stages:
+            if stage not in timepoint_annotations.values():
+                missing_stages.append(stage)
+        if len(missing_stages) > 0:
+            print('Position {} is missing the annotations for the following stages'.format(
+                position_name, missing_stages))
+            bad_positions.append(position_name)
+    return bad_positions
+        
+
 def print_formatted_list(string_list):
     return "[\'" + "\',\'".join(string_list) + "\']"
