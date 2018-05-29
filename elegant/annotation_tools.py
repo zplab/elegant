1c08ddc8ae90a86930246520dee88aa52c4a1eec
drew-sinha <>
2018-05-29 14:34:57 -0500

    Refactored filtering of excluded positions into separate function; incremental changes

--- elegant/annotation_tools.py
+++ elegant/annotation_tools.py
@@ -1,4 +1,4 @@
-def filter_positions(annotations, selection_criteria, invert_selection=False, filter_excluded=True):
+def filter_positions(annotations, selection_criteria, invert_selection=False):
     """Filter positions for an experiment based on defined selection criteria
 
     Parameters
@@ -22,11 +22,6 @@ def filter_positions(annotations, selection_criteria, invert_selection=False, fi
             for position in annotations.keys()
             if position not in selected_positions]
     
-    if filter_excluded:
-        selected_positions = [position
-            for position in selected_positions
-            if annotations[position][0]['exclude'] is False]
-    
     return selected_positions
 
 def filter_positions_by_kw(annotations, selection_kws, invert_selection=False):
@@ -43,12 +38,29 @@ def filter_positions_by_kw(annotations, selection_kws, invert_selection=False):
     def check_kws(position_annotations, selection_kws):
         global_annotations, timepoint_annotations = position_annotations
         return any([kw in global_annotations['notes'] for kw in selection_kws])
-    selection_by_kw = lambda position_annotations: check_kws(position_annotations, selection_kws)
+    select_by_kw = lambda position_annotations: check_kws(position_annotations, selection_kws)
     
     return filter_positions(
         annotations, 
-        selection_by_kw, 
+        select_by_kw, 
         invert_selection=invert_selection)
+
+def filter_excluded(annotations):
+    """Filter positions for an experiment based on exclusion flag in corresponding annotations
+
+    Parameters
+        annotations - An OrderedDict mapping position names to corresponding
+            annotations (returned by load_data.read_annotations)
+    """
+    
+    def select_excluded(position_annotations):
+        global_annotations, timepoint_annotations = position_annotations
+        return global_annotations['exclude'] is False
+    
+    return filter_positions(
+        annotations,
+        select_excluded)
+    
 
 def check_stage_annotations(annotations, stages):
     """Check that a set of annotations are complete 
@@ -68,11 +80,11 @@ def check_stage_annotations(annotations, stages):
         stage_annotations = [timepoint_annotation['stage'] 
             for timepoint_annotation in timepoint_annotations.values()]
         return all([stage in stage_annotations for stage in stages])
-    selection_by_stage_annotation = lambda position_annotations: check_for_stages(position_annotations, stages)
+    select_by_stage_annotation = lambda position_annotations: check_for_stages(position_annotations, stages)
     
     return filter_positions(
         annotations,
-        selection_by_stage_annotation,
+        select_by_stage_annotation,
         invert_selection=True) # Get positions whose stages are not all annotated
     
 def print_formatted_list(string_list):
