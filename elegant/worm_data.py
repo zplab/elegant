aab8d19acd7a6f0f184508db268a58df87726736
Zach Pincus <zpincus@gmail.com>
2018-10-17 11:32:04 -0500

    fix previous fix to z-transform

--- elegant/worm_data.py
+++ elegant/worm_data.py
@@ -1118,7 +1118,7 @@ class Worms(collections.UserList):
         worm_ages = [d[:, 0] for d in data]
         for worm, ages, data in zip(self, worm_ages, worm_data):
             worm_ages = age_feature(worm) if callable(age_feature) else getattr(worm.td, age_feature)
-            vals = numpy.empty(worm_ages, dtype=float)
+            vals = numpy.empty(worm_ages.shape, dtype=float)
             vals.fill(numpy.nan)
             age_mask = numpy.in1d(worm_ages, ages, assume_unique=True)
             vals[age_mask] = data
