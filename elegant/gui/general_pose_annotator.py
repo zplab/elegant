235e9b0020a95c7c5926626e6042d9c975254f12
nlaird <n.laird@wustl.edu>
2019-07-12 17:28:04 -0500

    Fixed things

--- elegant/gui/general_pose_annotator.py
+++ elegant/gui/general_pose_annotator.py
@@ -17,6 +17,16 @@ from zplib.image import colorize
 class GeneralPoseAnnotator():
     def __init__(self, rw):
         self.ris_widget = rw
+        width_estimator = worm_widths.WidthEstimator.from_default_widths(pixels_per_micron=1/1.3)
+        self.pose_annotator = pose_annotation.PoseAnnotation(self.ris_widget, width_estimator=width_estimator)
+        self.ris_widget.add_annotator([self.pose_annotator])
+
+        
+        load_data = Qt.QPushButton('Load')
+        load_data.clicked.connect(self.load_metadata)
+        save_data = Qt.QPushButton('Save')
+        save_data.clicked.connect(self.save_annotations)
+        self.pose_annotator._add_row(self.pose_annotator.widget.layout(), Qt.QLabel('Data:'),load_data, save_data)
 
     def save_annotations(self):
         """Save the pose annotations as pickle files into the parent directory.
@@ -27,8 +37,23 @@ class GeneralPoseAnnotator():
             annotations = getattr(fp, 'annotations', None)
             if annotations is not None:
                 pose = annotations.get('pose')
-                if pose is not None or pose is not (None, None):     
+                if pose is not None and pose is not (None, None):
                     path = pathlib.Path(fp[0].name)
+                    lab_frame_image = fp[0].data
+                    center_tck, width_tck = pose
+                    #warp and save the warped images
+                    if center_tck is not None:
+                        warp = worm_spline.to_worm_frame(lab_frame_image, center_tck, width_tck)
+                        warp_save_path = path.parent/(path.stem+"-straight.png")
+                        freeimage.write(warp, warp_save_path)
+
+                        #if the widths are drawn, then create a mask that allows the user to make an alpha channel later
+                        if width_tck is not None:
+                            worm_frame_mask = worm_spline.worm_frame_mask(width_tck, warp.shape)
+                            img_save_path = path.parent/(path.stem+"-mask.png")
+                            freeimage.write(worm_frame_mask, img_save_path)
+
+                    #save annotations into a pickle file
                     save_path = path.with_suffix('.pickle')
                     with save_path.open('wb') as f:
                         pickle.dump(annotations, f)
@@ -41,11 +66,11 @@ class GeneralPoseAnnotator():
             annotations = getattr(fp, 'annotations', None)
             if annotations is not None:    
                 path = pathlib.Path(fp[0].name)
-                img_name = path.name.split('.')[0]
                 
-                for ann_path in path.parent.glob(img_name+('.pickle')):
-                    annotations = pickle.load(open(ann_path, 'rb'))
-                    fp.annotations = annotations
+                annotation_path = path.with_suffix('.pickle')
+                with annotation_path.open('rb') as f:
+                    annotations = pickle.load(f)
+                fp.annotations = annotations
 
         self.ris_widget.annotator.update_fields()
 
@@ -53,30 +78,30 @@ class GeneralPoseAnnotator():
         """Save the warped images into the same parent directory as the images in the flipbook.
         If there is a width tck, the saved images have a transparent channel with the worm mask.
         The warped images will be saved with the same base name as the first image in the flipbook_page list
-        with "-straightening.png" afterwards.
+        with "-straight.png" afterwards.
 
         Example:
             image name: 'test.png'
-            straightened image name: 'test-straightened.png'
+            straightened image name: 'test-straight.png'
         """
         for fp in self.ris_widget.flipbook_pages:
             lab_frame_image = fp[0].data
             annotations = getattr(fp, 'annotations', None)
             if annotations is not None:
                 pose = annotations.get('pose')
-                if pose is not None or pose is not (None, None):
+                if pose is not None and pose is not (None, None):
                     center_tck, width_tck = pose
                     if center_tck is not None:
                         warp = worm_spline.to_worm_frame(lab_frame_image, center_tck, width_tck)
-                        warp = colorize.scale(warp).astype(numpy.uint8)
                         if width_tck is not None:
                             worm_frame_mask = worm_spline.worm_frame_mask(width_tck, warp.shape)
-                            image = numpy.dstack((warp, warp, warp, worm_frame_mask))
                         else:
                             image = numpy.dstack((warp, warp, warp))
                         path = pathlib.Path(fp[0].name)
-                        save_path = path.parent/(path.name.split(".")[0]+"-straightening.png") 
+                        save_path = path.stem+"-straight.png"
                         freeimage.write(image, save_path)
+
+
 
 def main(argv=None):
     import argparse
@@ -86,21 +111,10 @@ def main(argv=None):
     args = parser.parse_args(argv)
     
     rw = ris_widget.RisWidget()
-    width_estimator = worm_widths.WidthEstimator.from_default_widths(pixels_per_micron=1/1.3)
-    pa = pose_annotation.PoseAnnotation(rw, width_estimator=width_estimator)
-    rw.add_annotator([pa])
-
     gp = GeneralPoseAnnotator(rw)
-    load_data = Qt.QPushButton('Load')
-    load_data.clicked.connect(gp.load_metadata)
-    save_data = Qt.QPushButton('Poses')
-    save_data.clicked.connect(gp.save_annotations)
-    save_warps = Qt.QPushButton('Images')
-    save_warps.clicked.connect(gp.save_warped_images)
-    pa._add_row(pa.widget.layout(), Qt.QLabel('Data:'),load_data, save_data, save_warps)    
      
     rw.add_image_files_to_flipbook(args.images)
-    shared_resources._QAPPLICATION.exec()
+    rw.run()
 
 if __name__ == '__main__':
     main()
