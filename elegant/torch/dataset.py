914c47461876ddf11f469d20ab5ba767a51237c6
Zach Pincus <zpincus@gmail.com>
2020-07-17 17:06:22 -0500

    add documentation to torch dataset

--- elegant/torch/dataset.py
+++ elegant/torch/dataset.py
@@ -6,7 +6,36 @@ from elegant import process_images
 from elegant import worm_spline
 
 class WormDataset(data.Dataset):
+    """Adaptor from datamodel classes to Torch Dataset.
+
+    The general idea is that you provide the constructor with a sequence of
+    datamodel.Timepoint instances (generally this would be via constructing a
+    datamodel.Timepoints instance, but other methods will work), and a callable
+    generate_data function that takes a Timepoint and returns whatever Torch is
+    intended to work with (either a single image for testing/running a convnet,
+    or a image/target pair for training).
+
+    The generate_data function can either be a regular function, or a callable
+    class to enable parameters to be set to define the specific behavior of
+    the function.
+
+    Example:
+    experiment_paths = [...] # list of paths to experiment directories
+    experiments = [datamodel.Experiment(path) for path in experiment_paths]
+    test, train = datamodel.Timepoints.split_experiments(*experiments, fractions=[0.3, 0.7])
+
+    test_labframe_dataset = WormDataset(test, normalized_bf_image)
+
+    generate_worm_image = GenerateWormFrame(image_size=(1000,200))
+    test_wormframe_dataset = WormDataset(test, generate_worm_image)
+
+    """
     def __init__(self, timepoints, generate_data):
+        """Parameters:
+            timepoints: sequence of datamodel.Timepoint instances
+            generate_data: function (or callable class) that takes a Timepoint
+                instance and returns whatever PyTorch needs.
+        """
         super().__init__()
         self.timepoints = timepoints
         self.generate_data = generate_data
@@ -20,6 +49,7 @@ class WormDataset(data.Dataset):
 
 
 def normalized_bf_image(timepoint):
+    """Given a timepoint, return a normalized brightfield image."""
     bf = freeimage.read(timepoint.image_path('bf'))
     mode = process_images.get_image_mode(bf, optocoupler=timepoint.position.experiment.metadata['optocoupler'])
     # map image image intensities in range (100, 2*mode) to range (0, 2)
@@ -29,6 +59,10 @@ def normalized_bf_image(timepoint):
     return bf
 
 class GenerateWormFrame:
+    """Callable class that returns a worm-frame image when called with a Timepoint instance.
+
+    Shape of the worm-frame image can be configured at class initialization.
+    """
     def __init__(self, image_size):
         self.image_size = image_size
 
